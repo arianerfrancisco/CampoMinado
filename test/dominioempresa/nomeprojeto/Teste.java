c4f808e9d8e55e59cc2f9f844edaf1503632eb2a
Ariane Rocha <72419533+arianerfrancisco@users.noreply.github.com>
2021-12-08 09:18:56 -0300

    Teste unitário

---
+++ test/dominioempresa/nomeprojeto/Teste.java
@@ -0,0 +1,17 @@
+package dominioempresa.nomeprojeto;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+public class Teste {
+	@Test
+	void Test() {
+		
+		int a = 1 + 1;
+		
+		assertEquals(2, a);
+		
+	}
+
+}
